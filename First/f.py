63071694a130d3d834ddcc852338e19ceecbc31e
Tony Eappen <togmotiv@gmail.com>
2026-04-08 09:41:44 +0530

    first general purpose chatbot agent made with groq

--- First/f.py
+++ First/f.py
@@ -1,8 +1,29 @@
 from dotenv import load_dotenv
+from groq import Groq
 from os import getenv
 
 load_dotenv()
 
-apiKey = getenv('OPENAI_API_KEY')
+apiKey = getenv('GROQ_API_KEY')
 
-print(apiKey)
+gClient = Groq(api_key=apiKey)
+
+print("🤖 Type 'exit' to end the session ")
+while True: 
+    user_input = input('🙋 You: ')
+    if user_input.lower() == 'exit':
+        break
+
+    response = gClient.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+        {"role": "system", "content": "You are helpful chatbot"},
+        {"role": "user", "content": user_input}
+        ],
+        stream=True
+    )
+    
+    print("🤖 Bot: ", end="")
+    for chunk in response:
+        print(chunk.choices[0].delta.content or "", end="", flush=True)
+    print('\n')
